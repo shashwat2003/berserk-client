0246af774018f13bc235a9f771f1d048d583e666
shashwat2003 <shashwat13.8@gmail.com>
2025-04-23 20:39:38 +0530

    feat: not-found route

---
+++ app/+not-found.tsx
@@ -0,0 +1,11 @@
+import { Typography } from '@/tamagui/components/typography'
+import { YStack } from 'tamagui'
+
+export function NotFound() {
+  return (
+    <YStack flex={1} justifyContent="center" alignItems="center">
+      <Typography.Title>404 - Not Found</Typography.Title>
+      <Typography.Paragraph>The page you are looking for does not exist.</Typography.Paragraph>
+    </YStack>
+  )
+}
